ecfc166c12a08fb617567a853e1db3bc0ebbd0dc
saurabhofficialjava <saurabhofficialjava@gmail.com>
2024-12-23 17:20:02 +0530

    Adding Gateway,LBS and Business services

---
+++ services/lbs/src/main/java/com/sau/lbs/LbsApplication.java
@@ -0,0 +1,13 @@
+package com.sau.lbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbsApplication.class, args);
+	}
+
+}
---
+++ services/lbs/src/test/java/com/sau/lbs/LbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sau.lbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
